1a36af0e64736eb2751b18d48243037cfe2d9a16
lmunozv <luis.munoz@bizagi.com>
2017-03-22 14:40:02 -0500

    Se adjunta al catalogo el ejecutor de reglas

---
+++ Bizagi.Catalogo.Funcionalidades/Bizagi.Ejecutor.Reglas/Ejecutar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Bizagi.Ejecutor.Reglas
+{
+    public class Ejecutar
+    {
+        public object MethodRuleEjecutor(string clase, string nSpace_Clase, string metodo, ArrayList argumentos)
+        { 
+          return RuleManager.MethodRuleEjecutor(clase, nSpace_Clase, metodo, GetArguments(argumentos));        
+        }
+
+        private object[] GetArguments(ArrayList argumentos)
+        {
+            object[] response = new object[argumentos.Count];
+            int x = 0;
+            foreach (var item in argumentos)
+            {
+                response[x] = item;
+            }
+
+            return response;
+        }
+
+        private ReglaBO ObtenerRegla(string clase)
+        {
+            ReglaBO regla = new ReglaBO();
+            string nombreClase = string.Empty;
+            string met = string.Empty;
+            string cs = string.Empty;
+            string nameSpace = string.Empty;
+            cs = @"using System;
+                                using System.Collections.Generic;
+                                using System.Text;  
+                                using System.Diagnostics;                              
+                                namespace Bizagi.Ejecutor.Reglas
+                                {
+                                  @reg
+                                };".Replace("@reg", clase.ToString());
+            nameSpace = "Bizagi.Ejecutor.Reglas." + nombreClase;
+            regla.CS = cs;
+            regla.Metodo = met;
+            regla.NameSpace = nameSpace;
+            return regla;
+        }
+    }
+
+    
+
+    public class ReglaBO
+    {
+        public string Metodo { get; set; }
+        public string CS { get; set; }
+        public string NameSpace { get; set; }
+    }
+}
---
+++ Bizagi.Catalogo.Funcionalidades/Bizagi.Ejecutor.Reglas/RuleManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.CodeDom.Compiler;
+
+namespace Bizagi.Ejecutor.Reglas
+{
+    public class RuleManager
+    {
+        public static object MethodRuleEjecutor(string clase, string nSpace_Clase, string metodo, object[] argumentos)
+        {
+            object objClase = Compiler(clase, nSpace_Clase);
+            if (objClase.ToString().Contains("error"))
+            {
+                return objClase;
+            }
+            else
+            {
+                MethodInfo main;
+                if (objClase.GetType().GetMethod(metodo).IsGenericMethod)
+                {
+                    Type[] argm = objClase.GetType().GetMethod(metodo).GetGenericArguments();
+                    main = objClase.GetType().GetMethod(metodo).MakeGenericMethod(argm);
+                    return main.Invoke(null, argumentos);
+                }
+                else
+                {
+                    main = objClase.GetType().GetMethod(metodo);
+                    return main.Invoke(null, argumentos);
+                }
+
+
+                //return objClase.GetType().InvokeMember(metodo, BindingFlags.InvokeMethod, null, objClase, argumentos);
+                //MethodInfo main = objClase.GetType().GetMethod(metodo);
+                //return main.Invoke(null, null);
+            }
+
+        }
+
+
+        public static object Compiler(string clase, string nSpace_Clase)
+        {
+            try
+            {
+                #region Parametros
+                CompilerParameters objParametros = new CompilerParameters();
+                //if (Convert.ToBoolean(ConfigurationManager.AppSettings["Debug"]))
+                //{
+                //    objParametros.GenerateInMemory = true;
+                //    objParametros.IncludeDebugInformation = true;
+                //}
+                //else
+                //{
+                    objParametros.GenerateInMemory = false;
+                    objParametros.IncludeDebugInformation = false;
+                //}
+                objParametros.GenerateExecutable = false;
+                #endregion
+
+                objParametros.TempFiles.KeepFiles = true;
+                CodeDomProvider objCompiler = CodeDomProvider.CreateProvider("CSharp");
+                //foreach (AssemblyName reference in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
+                //{
+                //    if (reference.Name.Contains("DevExpress"))
+                //    {
+                //        objParametros.ReferencedAssemblies.Add(ConfigurationManager.AppSettings["URL"] + reference.Name + ".dll");
+                //    }
+                //    else if (reference.Name.Contains("AutoMapper"))
+                //    {
+                //        objParametros.ReferencedAssemblies.Add(ConfigurationManager.AppSettings["URLMapper"] + reference.Name + ".dll");
+                //    }
+                //    else
+                //    {
+                //        objParametros.ReferencedAssemblies.Add(reference.Name + ".dll");
+                //    }
+
+                //}
+                objParametros.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+                CompilerResults objResultados = objCompiler.CompileAssemblyFromSource(objParametros, clase);
+                if (objResultados.Errors.HasErrors)
+                {
+                    StringBuilder sbError = new StringBuilder();
+                    foreach (var item in objResultados.Errors)
+                    {
+                        sbError.Append(item);
+                        sbError.Append("\n");
+                    }
+                    return sbError;
+                }
+                Assembly assembly = objResultados.CompiledAssembly;
+                object objClase = objResultados.CompiledAssembly.CreateInstance(nSpace_Clase,
+                    false, BindingFlags.CreateInstance, null, null, null, null);
+                return objClase;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
