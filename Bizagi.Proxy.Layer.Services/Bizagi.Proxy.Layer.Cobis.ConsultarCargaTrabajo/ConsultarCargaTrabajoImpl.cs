b58fbbf0aec4ba448747fe7946143c6f445df9b7
AndresY <AndresY@SER-ANDRESY>
2017-06-27 09:55:27 -0500

    Se adiciona Proxy Consulta Carga Trabajo (Service reference, Operación en Manager). Pendiente el Servicio.

---
+++ Bizagi.Proxy.Layer.Services/Bizagi.Proxy.Layer.Cobis.ConsultarCargaTrabajo/ConsultarCargaTrabajoImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Xml;
+using Bizagi.Proxy.Layer.Cobis.ConsultarCargaTrabajo.Credito_ConsultarCargaTrabajo;
+using Bizagi.Proxy.Layer.Util;
+
+namespace Bizagi.Proxy.Layer.Cobis.ConsultarCargaTrabajo
+{
+    public class ConsultarCargaTrabajoImpl
+    {
+        public etapaEstacionType[] consultarCargaTrabajo(consultarCargaTrabajoPorEtapa_Input input)
+        {
+            try
+            {
+                ProxyUtils.ByPassCertificate();
+                Credito_ConsultarCargaTrabajoPortTypeClient ClientWs =
+                 new Credito_ConsultarCargaTrabajoPortTypeClient();
+                ClientWs.ClientCredentials.UserName.UserName = ProxyUtils.GetServiceUser("UsrServices");
+                ClientWs.ClientCredentials.UserName.Password = ProxyUtils.GetServicePwd("PwdServices");
+                using (OperationContextScope scope = new OperationContextScope(ClientWs.InnerChannel))
+                {
+                    OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] =
+                        ProxyUtils.GetHttpRequestMessageProperty();
+                    var SomeResposne = ClientWs.consultarCargaTrabajoPorEtapa(input);
+                    return SomeResposne.ConsultarCargaTrabajoPorEtapaRs;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }   
+}
